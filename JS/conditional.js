0b79dd898e6a824b062b442d1b67f9f67ef946bf
subhamgcon <subhamgcon@gmail.com>
2022-07-04 22:17:27 +0530

    conditional

---
+++ JS/conditional.js
@@ -0,0 +1,10 @@
+// Baby weather app(conditionals)
+
+let weather = prompt('How is the weather?')
+if(weather == 'rainy')
+{
+  console.log("Grab your umbrella ☔️ ")
+}
+else {
+  console.log("Wear your sunglasses 😎")
+}
