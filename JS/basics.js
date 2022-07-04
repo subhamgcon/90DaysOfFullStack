fa0b8071370d2afd64c5427c2ff02f438f4fed9b
subhamgcon <subhamgcon@gmail.com>
2022-07-04 21:52:00 +0530

    hello

---
+++ JS/basics.js
@@ -0,0 +1,20 @@
+console.log('Hello world')
+console.log('Glitch')
+// variables
+
+
+sentence = 'Hello there, General kenobi'
+
+console.log(sentence);
+// operators
+food = Number(prompt('How much was the food?'))
+// prompt is used to take input from the user
+
+tipPer = Number(prompt('tip %?') / 100)
+
+tipAmt = food * tipPer
+total = tipAmt + food
+console.log("Total:",total)
+console.log("Tip Amount:",tipAmt)
+// alert
+//alert(tipAmt)
